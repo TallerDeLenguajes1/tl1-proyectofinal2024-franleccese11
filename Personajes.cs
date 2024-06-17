1a55ef3471409d324f1116379439d96393999913
franleccese11 <franchelaleccese@gmail.com>
2024-06-17 18:08:35 -0300

    añadido el metodo para obtener informacion de la API

--- Personajes.cs
+++ Personajes.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+using System.Threading.Tasks;
 namespace espacioPersonaje
 {
     enum Tipo
@@ -7,11 +11,50 @@ namespace espacioPersonaje
 		quincy,
 		arrancar
     }
-    
+
+    public class PersonajesJSON
+    {
+        [JsonPropertyName("id")]
+         public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public Name Name{ get; set;}
+
+        [JsonPropertyName("gender")]
+        public string Gender{ get; set;}
+
+        [JsonPropertyName("height")]
+        public string height{ get; set;}
+
+        [JsonPropertyName("height")]
+        public string weight{ get; set;}
+
+
+
+    }
+
+    public class Name
+    {
+        [JsonPropertyName("english")]
+        public string English { get; set; }
+
+        [JsonPropertyName("kanji")]
+        public string Kanji { get; set; }
+
+        [JsonPropertyName("romaji")]
+        public string Romaji { get; set; }
+    }
+
     public class Personajes
     {   
         private Datos datos;
         private Caracteristicas caracteristicas;
+
+        public Personajes(Datos datos,Caracteristicas caracteristicas)
+        {
+            this.caracteristicas = caracteristicas;
+            this.datos = datos;
+        }
     }
 
     public class Datos
@@ -19,7 +62,7 @@ namespace espacioPersonaje
         private Tipo tipo;
         private string nombre;
         private string genero;
-        private string? cumpleaños;
+        private string cumpleaños;
 
         private string altura;
 
@@ -33,8 +76,24 @@ namespace espacioPersonaje
         private int destreza;   //1 a 5
         private int fuerza;  //1 a 10
 
-        private int nivel; //1 a 10
+        private int nivelReatsu; //1 a 10
         private int armadura; //1 a 100
         private int salud; //1 a 100
     }
+
+    public class obtenerPersonajesAPI
+    {
+        private static readonly HttpClient client = new HttpClient();
+         private static async Task<List<PersonajesJSON>> GetPjJSONs()
+         {
+            var url = "https://bleach-api-8v2r.onrender.com/characters";
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); 
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<PersonajesJSON> listaPersonajes = JsonSerializer.Deserialize<List<PersonajesJSON>>(responseBody);
+            return listaPersonajes;
+         }
+    }
 }
+
+
