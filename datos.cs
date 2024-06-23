f8891bae4ab4698bd0f54852bcd67eca67eaad6a
franleccese11 <franchelaleccese@gmail.com>
2024-06-23 17:12:12 -0300

    metodo para leer archivo de personajes añadido

--- datos.cs
+++ datos.cs
@@ -19,5 +19,53 @@ namespace datos
                 }
             }
         }
+
+        public static List<Personaje> LeerPersonajes(string nombreArchivo)
+        {
+            using (FileStream archivo = new FileStream(nombreArchivo,FileMode.Open))
+            {
+                using (StreamReader strReader = new StreamReader(archivo))
+                {
+                    string listaPersonajesJson = strReader.ReadToEnd();
+                    archivo.Close();
+
+                    Console.WriteLine("lista de personajes encontrada con exito!");
+
+                    List <Personaje>listaPersonajes = JsonSerializer.Deserialize<List<Personaje>>(listaPersonajesJson);
+
+                    return listaPersonajes;
+                }
+            }
+        }
+
+        public static bool Existe(string nombreArchivo)
+        {
+            if (File.Exists(nombreArchivo))
+            {
+                using (FileStream archivo = new FileStream(nombreArchivo, FileMode.Open))
+                {
+                    using (StreamReader StrReader = new StreamReader(archivo))
+                    {
+                        string listaPersonajesJson = StrReader.ReadToEnd();
+                        archivo.Close();
+
+                        if (listaPersonajesJson != null)
+                        {
+                            return true; //tiene datos y existe 
+                        }else
+                        {
+                            return false;  //no tiene datos
+                        }
+                    }
+                }
+            }else
+            {
+                return false; //no existe el archivo
+            }
+        }
+
+    
+
+
     }
 }
